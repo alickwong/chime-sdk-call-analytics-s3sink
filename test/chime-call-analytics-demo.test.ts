9afd159cd30338db460ecbbb619d8bc00388120b
Alick Wong <alickw@amazon.com>
2023-10-01 17:23:07 +0800

    Initial commit

---
+++ test/chime-call-analytics-demo.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ChimeCallAnalyticsDemo from '../lib/chime-call-analytics-demo-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/chime-call-analytics-demo-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ChimeCallAnalyticsDemo.ChimeCallAnalyticsDemoStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
